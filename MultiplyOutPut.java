7f21938da92abb26c65491bf4fd9a212de0702b8
Hardikgithub2020 <hardik.unity@gmail.com>
2020-08-24 19:48:30 -0400

    Add java files and ScreenShot Image

---
+++ MultiplyOutPut.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class MultiplyOutPut {
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		System.out.println("Enter value of first integer a :");
+		int a = in.nextInt();
+		System.out.println("Enter value of first integer b :");
+		int b = in.nextInt();
+		
+		System.out.println(" Multiplication value of both integer : "+(a*b));
+	}
+
+}
